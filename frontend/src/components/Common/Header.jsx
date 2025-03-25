a689323c71a43df9bdda10ea1963129b80153782
Boufasa Mourad <100412477+MouradBoufasa@users.noreply.github.com>
2025-03-25 22:41:45 +0100

    First layouts

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Topbar from "../Layout/Topbar";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <header>
+      <Topbar />
+      <Navbar />
+      {/*Cart drawer*/}
+    </header>
+  );
+};
+
+export default Header;
