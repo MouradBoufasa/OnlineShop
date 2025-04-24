d7e5dd6a6bb59e79c23b463e64de26bbfcab1191
Boufasa Mourad <100412477+MouradBoufasa@users.noreply.github.com>
2025-04-24 12:05:10 +0100

    creating product routes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -27,4 +27,12 @@ const protect = async (req, res, next) => {
   }
 };
 
-module.exports = { protect };
+const admin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(403).json({ message: "NOT AUTHORIZED AS AN ADMIN" });
+  }
+};
+
+module.exports = { protect, admin };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Product = require("../models/Product");
-const { protect } = require("../middleware/authMiddleware");
+const { protect, admin } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ const router = express.Router();
 //Create a new product
 //Access private/admin
 
-router.post("/", protect, async (req, res) => {
+router.post("/", protect, admin, async (req, res) => {
   try {
     const {
       name,
@@ -59,6 +59,96 @@ router.post("/", protect, async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(400).send("SERVER ERROR");
+  }
+});
+
+// PUT /api/products/:id
+// Update an existing product ID
+// Private/Admin
+
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      length,
+      width,
+      height,
+      weight,
+      sku,
+    } = req.body;
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      //Update product fields
+      product.name = name || product.name;
+      product.description = description || product.description;
+      product.price = price || product.price;
+      product.discountPrice = discountPrice || product.discountPrice;
+      product.countInStock = countInStock || product.countInStock;
+      product.category = category || product.category;
+      product.brand = brand || product.brand;
+      product.sizes = sizes || product.sizes;
+      product.colors = colors || product.colors;
+      product.collections = collections || product.collections;
+      product.material = material || product.material;
+      product.gender = gender || product.gender;
+      product.images = images || product.images;
+      product.isFeatured =
+        isFeatured !== undefined ? isFeatured : product.isFeatured;
+      product.isPublished =
+        isPublished !== undefined ? isPublished : product.isPublished;
+      product.tags = tags || product.tags;
+      product.dimensions = dimensions || product.dimensions;
+      product.dimensions.length = length || product.dimensions.length;
+      product.dimensions.width = width || product.dimensions.width;
+      product.dimensions.height = height || product.dimensions.height;
+      product.weight = weight || product.weight;
+      product.sku = sku || product.sku;
+
+      const updatedProduct = await product.save();
+      res.json(updatedProduct);
+    } else {
+      res.status(404).json({
+        message: "Product not found",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("SERVER ERROR");
+  }
+});
+
+// DELETE /api/:id
+// DELETE A PRODUCT By ID
+
+router.delete("/:id", protect, admin, async (req, res) => {
+  try {
+    // Find Product by ID
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      await product.deleteOne();
+      res.json({ message: "product removed with success" });
+    } else {
+      res.status(404).json({ message: "Product not found or already deleted" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("SERVER ERROR");
   }
 });
 
